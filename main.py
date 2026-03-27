cc8ec536ffd042a710d2a07d1f2b9e6f35e0e5ac
Johan Paul <johanjohn405@gmail.com>
2026-03-27 22:45:14 +0530

    Get method defined

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 app = FastAPI()
 db = {}
 
@@ -7,3 +8,10 @@ def shorten(url: str):
     code = str(len(db) + 1)
     db[code] = url
     return {"short": code}
+
+@app.get("/{code}")
+def redirect(code: str):
+    url = db.get(code)
+    if not url:
+        return 404
+    return RedirectResponse(url)
