e8e60dc9db9099b6e5a5e025749d2e247ef50d58
Johan Paul <johanjohn405@gmail.com>
2026-03-30 23:53:28 +0530

    Refactored Post Endpoint

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from fastapi.responses import RedirectResponse
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from database import engine
-from models import Base
 from models import URL
+from pydantic import BaseModel, HttpUrl
+from typing import Optional
 
 app = FastAPI()
+
+class URLRequest(BaseModel):
+    url: HttpUrl
+    custom_code: Optional[str] = None
 
 def encode(num: int):
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -22,14 +26,31 @@ def encode(num: int):
     return string[::-1]
 
 @app.post("/shorten")
-def shorten(url: str, db: Session = Depends(get_db)):
-    new_url = URL(long_url=url)
+def shorten(request: URLRequest, db: Session = Depends(get_db)):
+
+    if request.custom_code:
+        existing = db.query(URL).filter(URL.short_code == request.custom_code).first()
+        if existing:
+            raise HTTPException(status_code=400, detail="Code already taken")
+
+        new_url = URL(
+            long_url=str(request.url),
+            short_code=request.custom_code
+        )
+        db.add(new_url)
+        db.commit()
+
+        return {"short": request.custom_code}
+
+    new_url = URL(long_url=str(request.url))
     db.add(new_url)
     db.commit()
     db.refresh(new_url)
+
     code = encode(new_url.id)
-    new_url.short_code=code
+    new_url.short_code = code
     db.commit()
+
     return {"short": code}
 
 @app.get("/{code}")
